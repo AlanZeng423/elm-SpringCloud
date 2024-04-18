e1116a5617c7f6cf818170adc3a28d30aa51e511
Yhy <yinhongyang546@gmail.com>
2024-04-18 22:54:06 +0800

    update

---
+++ elm-backend-service-config/src/main/java/com/enusoft/elmbackendserviceconfig/ElmBackendServiceConfigApplication.java
@@ -0,0 +1,13 @@
+package com.enusoft.elmbackendserviceconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElmBackendServiceConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElmBackendServiceConfigApplication.class, args);
+    }
+
+}
---
+++ elm-backend-service-config/src/test/java/com/enusoft/elmbackendserviceconfig/ElmBackendServiceConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enusoft.elmbackendserviceconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElmBackendServiceConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
